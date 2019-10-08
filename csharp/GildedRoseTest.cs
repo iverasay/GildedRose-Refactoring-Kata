664fc86a72682fb8dfb72b26101274743ff86171
Ignacio Verasay <iverasay@users.noreply.github.com>
2019-10-08 17:48:04 +0200

    New test - Normal Items Quality After 5 Days
    Check the quality of Normals products after 5 day.

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -23,5 +23,21 @@ namespace csharp
             Assert.AreEqual(-2, Items[2].SellIn);
         }
 
+        [Test]
+        public void NormalItemsQualityAfter5Days()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "prod 1", SellIn = 1, Quality = 50 },
+                                                 new Item { Name = "prod 2", SellIn = 5, Quality = 10 },
+                                                 new Item { Name = "prod 3", SellIn = 3, Quality = 10 }};
+
+            GildedRose app = new GildedRose(Items);
+
+            for (int i = 0; i < 5; i++) app.UpdateQuality();
+
+            Assert.AreEqual(41, Items[0].Quality);
+            Assert.AreEqual(5, Items[1].Quality);
+            Assert.AreEqual(3, Items[2].Quality);
+        }
+
     }
 }
