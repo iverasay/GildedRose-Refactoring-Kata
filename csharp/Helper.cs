9086087069776dc7afab75ee22179f2c9d3b6fb2
Ignacio Verasay <iverasay@users.noreply.github.com>
2019-10-08 17:18:36 +0200

    New Class Helper
    This class helps identify what type of product it is. to act accordingly.

---
+++ csharp/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class Helper
+    {
+        public enum ProductType
+        {
+            Normal = 0,
+            Sulfuras = 1,
+            AgedBrie = 2,
+            BackstagePasses = 3,
+            Conjured = 4
+        }
+
+        public static int GetProductType(string productName)
+        {
+            if (productName.ToLower().Contains("sulfuras")) return (int)ProductType.Sulfuras;
+            if (productName.ToLower().Contains("aged brie")) return (int)ProductType.AgedBrie;
+            if (productName.ToLower().Contains("backstage passes")) return (int)ProductType.BackstagePasses;
+            if (productName.ToLower().Contains("conjured")) return (int)ProductType.Conjured;
+            return (int)ProductType.Normal;
+        }
+    }
+}
